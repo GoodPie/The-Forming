9007017a77337fa7a6b5a34f37cb2556cfdfcf3b
Brandyn Britton <brandynbb96@gmail.com>
2018-06-16 12:00:34 +1000

    Got working with Python 3
    - Changed print statements
    - Fixed file formatting
    - Removes ome redundant files

--- functions.py
+++ functions.py
@@ -1,26 +1,29 @@
 """This is where all primary, "non-game" related functions are helf"""
 import time
+
 from EntityClasses import *
 
-#Screen shot functionality
+
+# Screen shot functionality
 def take_screen_shot(screen):
     time_taken = time.asctime(time.localtime(time.time()))
     time_taken = time_taken.replace(" ", "_")
     time_taken = time_taken.replace(":", ".")
     save_file = "screenshots/" + time_taken + ".png"
     pygame.image.save(screen, save_file)
-    print "A screen shot has been taken and saved as: " + save_file
-    
-    
+    print("A screen shot has been taken and saved as: " + save_file)
 
-#This is a test to load images into a dictionary for faster loading
+
+# This is a test to load images into a dictionary for faster loading
 def get_image(key, cache):
     if not key in cache:
         cache[key] = pygame.image.load(key).convert_alpha()
     return cache[key]
 
+
 def currentLevelChecker(area="generation"):
     return area
+
 
 class Camera(object):
 
@@ -32,8 +35,7 @@ class Camera(object):
         return target.rect.move(self.state.topleft)
 
     def update(self, target):
-            self.state = self.camera_func(self.state, target.rect)
+        self.state = self.camera_func(self.state, target.rect)
 
     def reverse(self, pos):
         return (pos[0] - self.state.left, pos[1] - self.state.top)
-
