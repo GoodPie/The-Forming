f915fd3b5db02bf3b5edde0d79fb20e09cd1ff28
Brandyn Britton <brandynbb96@gmail.com>
2025-08-12 09:22:07 +0800

    update(tidy): Removed legacy classes
    Signed-off-by: Brandyn Britton <brandynbb96@gmail.com>

--- Entity.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-
-# Main Entity Class
-class Entity(pygame.sprite.Sprite):
-
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
--- Tiles/Foundation.py
+++
@@ -1,7 +0,0 @@
-from Tiles.Tile import Tile
-
-
-class Foundation(Tile):
-
-    def __init__(self, x, y, img):
-        Tile.__init__(self, x, y, "Foundation", img)
